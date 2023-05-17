3ef07592fdb75592bfb279c127b383c6cf1d240c
anxods <anxo500s@gmail.com>
2023-05-17 14:38:23 +0200

    Primera versión

---
+++ src/extract_spotify_data.py
@@ -0,0 +1,76 @@
+import secrets_spotify as ss
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from datetime import datetime
+import csv
+import os
+
+# Credentials to access Spotify API
+client_id = ss.CLIENT_ID
+client_secret = ss.CLIENT_SECRET
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def extract_data(playlist_id, country):
+
+    # Get all songs from playlist
+    results = sp.playlist_tracks(playlist_id)
+    tracks = results['items']
+    while results['next']:
+        results = sp.next(results)
+        tracks.extend(results['items'])
+
+    # Extract URIs of all songs composing the playlist
+    uris = [track['track']['uri'] for track in tracks]
+
+    # Obtaining tracks data and sorting it by field popularity (indicator of streaming count)
+    ## sorted_tracks = sorted(uris, key=lambda uri: sp.track(uri)['popularity'], reverse=True)
+
+    # Creating list of lists and including the header of the final CSV file 
+    spotify_data = []
+    head = ['date', 'position', 'song', 'artist', 'popularity', 'duration_ms', 'album_type', 'total_tracks', 'release_date', 'is_explicit', 'album_cover_url']
+    ## spotify_data.append(head)
+    today = datetime.today().strftime('%Y-%m-%d')
+
+    # Getting the data obtained from Spotify API into the list of lists
+    for rank, uri in enumerate(uris):
+        track = sp.track(uri)
+
+        song = track['name']
+        artist_aux = [artist['name'] for artist in track['album']['artists']]
+        if len(artist_aux) > 1:
+            artist = artist_aux[0] + ' & ' + ' & '.join(artist_aux[1:])
+        else:
+            artist = artist_aux[0]
+        popularity = track['popularity']
+        duration_ms = track['duration_ms']
+        album_type = track['album']['album_type']
+        total_tracks = track['album']['total_tracks']
+        release_date = track['album']['release_date']
+        is_explicit = track['explicit']
+        album_cover_url = track['album']['images'][0]['url']
+
+        spotify_data.append([today, rank+1, song, artist, popularity, duration_ms, album_type, total_tracks, release_date, is_explicit, album_cover_url])
+
+    ## Pass data into a csv file
+    csv_file_path = './data/spotify-streaming-top-50-' + country + '.csv'
+
+    date_processed = False
+    if os.path.exists(csv_file_path):
+        with open(csv_file_path, 'r', encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row['date'] == today:
+                    date_processed = True
+                    break
+
+    if not date_processed:
+        if not os.path.exists(csv_file_path):
+            with open(csv_file_path, 'w', encoding="utf-8", newline='') as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerow(head)
+
+        with open(csv_file_path, 'a', encoding="utf-8", newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            for data_list in spotify_data:
+                writer.writerow(data_list)
---
+++ src/main.py
@@ -0,0 +1,31 @@
+import extract_spotify_data as esd
+
+## Top 50: World - Data
+esd.extract_data('37i9dQZEVXbMDoHDwVN2tF', 'world')
+
+## Top 50: USA - Data
+esd.extract_data('37i9dQZEVXbLRQDuF5jeBp', 'usa')
+
+## Top 50: Spain - Data
+esd.extract_data('37i9dQZEVXbNFJfN1Vw8d9', 'spain')
+
+## Top 50: UK - Data
+esd.extract_data('37i9dQZEVXbLnolsZ8PSNw', 'uk')
+
+## Top 50: Italy - Data
+esd.extract_data('37i9dQZEVXbIQnj7RRhdSX', 'italy')
+
+## Top 50: France - Data
+esd.extract_data('37i9dQZEVXbIPWwFssbupI', 'france')
+
+## Top 50: Mexico - Data
+esd.extract_data('37i9dQZEVXbO3qyFxbkOE1', 'mexico')
+
+## Top 50: Argentina - Data
+esd.extract_data('37i9dQZEVXbMMy2roB9myp', 'argentina')
+
+## Top 50: Japan - Data
+esd.extract_data('37i9dQZEVXbKXQ4mDTEBXq', 'japan')
+
+## Top 50: South Korea - Data
+esd.extract_data('37i9dQZEVXbNxXF4SkHj9F', 'south-korea')
